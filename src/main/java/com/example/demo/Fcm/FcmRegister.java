6dd41f8e433d2fb524b6ff89b0bafeaa6c50bceb
Nohbin2 <bean__2@naver.com>
2024-09-02 15:06:10 +0900

    [feature]Notification send

--- src/main/java/com/example/demo/Fcm/FcmRegister.java
+++ src/main/java/com/example/demo/Fcm/FcmRegister.java
@@ -1,6 +1,5 @@
 package com.example.demo.Fcm;
 
-import com.example.demo.Notification.NotificationRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,13 +10,13 @@ public class FcmRegister {
     private final FcmService fcmService;
 
     public FcmRegister(FcmService fcmService) {
-
         this.fcmService = fcmService;
     }
 
     @PostMapping("/register")
     public ResponseEntity<Response> sendFcm(@RequestBody FcmRequest fcmRequest) {
         try {
+            System.out.println("token :"+ fcmRequest.getFcmToken() + " " + fcmRequest.getPhoneNumber());
             //DB 저장
             fcmService.saveFcmToken(fcmRequest.getPhoneNumber(), fcmRequest.getFcmToken());
             // JSON 응답을 반환
@@ -28,4 +27,17 @@ public class FcmRegister {
             return ResponseEntity.status(500).body(new Response("error"));
         }
     }
+
+    @PostMapping("/send")
+    public ResponseEntity<Response> sendNotification(@RequestBody NotificationRequest notificationRequest) {
+        try {
+            // 알림 전송
+            fcmService.sendNotification(notificationRequest.getPhoneNumber(), notificationRequest.getTitle(), notificationRequest.getBody());
+            return ResponseEntity.ok(new Response("notification sent"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body(new Response("error"));
+        }
+    }
 }
+
