fc675f8ec8974348379e8ccee0af80b4b491abbd
Nohbin2 <bean__2@naver.com>
2024-10-16 14:30:35 +0900

    [feature]Profile Image URL [feature]checkNumber return value change

--- src/main/java/com/example/demo/Fcm/FcmRequest.java
+++ src/main/java/com/example/demo/Fcm/FcmRequest.java
@@ -1,17 +1,23 @@
 package com.example.demo.Fcm;
 
+import java.util.List;
+
 public class FcmRequest {
     private String phoneNumber;
     private String fcmToken;
+    private String name;
+    private List<String> phoneNumbers;
+    private String imageUrl;
 
     // 기본 생성자 (필수)
     public FcmRequest() {
     }
 
     // 전체 필드 생성자
-    public FcmRequest(String phoneNumber, String fcmToken) {
+    public FcmRequest(String phoneNumber, String fcmToken, String name) {
         this.phoneNumber = phoneNumber;
         this.fcmToken = fcmToken;
+        this.name = name;
     }
 
     // Getter 및 Setter
@@ -29,5 +35,26 @@ public class FcmRequest {
 
     public void setFcmToken(String fcmToken) {
         this.fcmToken = fcmToken;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public List<String> getPhoneNumbers() {
+        return phoneNumbers;
+    }
+
+    public void setPhoneNumbers(List<String> phoneNumbers) {
+        this.phoneNumbers = phoneNumbers;
+    }
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
     }
 }
